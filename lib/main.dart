f583f2378eaed937f64a2681ef0ad0915afe90d3
Kazuma-Te <takazu0514@gmail.com>
2020-08-10 15:37:36 +0900

    webviewの一応の実装、チャット欄の画面作成

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_school_app/Login.dart';
 import 'package:flutter_school_app/Message.dart';
 import 'package:flutter_school_app/login_page.dart';
 import 'package:flutter_school_app/signup_page.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 import 'book_list_page.dart';
 
@@ -26,103 +27,90 @@ class _State extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('S I S'),
-        ),
-        drawer: Drawer(
-          child: ListView(
-            children: <Widget>[
-              DrawerHeader(
-                child: Text(
-                  'Sharing Information at School',
-                  style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.white,
-                  ),
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
+      appBar: AppBar(
+        title: Text('S I S'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              child: Text(
+                'Sharing Information at School',
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white,
                 ),
               ),
-              ListTile(
-                title: Text('ホームページ'),
-                onTap: () {
-                  setState(() => _city = 'Los Angeles, CA');
-                  Navigator.pop(context);
-                },
+              decoration: BoxDecoration(
+                color: Colors.blue,
               ),
-              ListTile(
-                title: Text('学校通信'),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => BookListPage()),
-                  );
+            ),
+            ListTile(
+              title: Text('ホームページ'),
+              onTap: () {
+                setState(() => _city = 'Los Angeles, CA');
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('学校通信'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => BookListPage()),
+                );
 //                  setState(() => _city = 'Honolulu, HI');
 //                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: Text('メッセージ'),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Message()),
-                  );
-                  //Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: Text('ログイン'),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => LoginPage()),
-                  );
-                },
-              ),
-              ListTile(
-                title: Text('新規登録'),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SignUpPage()),
-                  );
-                },
-              ),
-              ListTile(
-                title: Text(
-                  'ログアウト',
-                  style: TextStyle(
-                    color: Colors.red,
-                  ),
+              },
+            ),
+            ListTile(
+              title: Text('メッセージ'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ChatPage('かずま')),
+                );
+                //Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('ログイン'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                );
+              },
+            ),
+            ListTile(
+              title: Text('新規登録'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SignUpPage()),
+                );
+              },
+            ),
+            ListTile(
+              title: Text(
+                'ログアウト',
+                style: TextStyle(
+                  color: Colors.red,
                 ),
-                onTap: () {
-                  setState(() => _city = 'Tokyo, Japan');
-                  Navigator.pop(context);
-                },
               ),
-            ],
-          ),
+              onTap: () {
+                setState(() => _city = 'Tokyo, Japan');
+                Navigator.pop(context);
+              },
+            ),
+          ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'SISへようこそ',
-                style: TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-              Text(
-                _city,
-                style: TextStyle(
-                  fontSize: 32,
-                ),
-              ),
-            ],
-          ),
-        ));
+      ),
+      body: WebView(
+        initialUrl: 'https://www.pu-toyama.ac.jp',
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController controller) {},
+      ),
+    );
   }
 }
